56a37d8229133dc78898cc59508fa57c094161b3
pratyushvox <np03cs4s230182@heraldcollege.edu.np>
2025-12-09 12:45:28 +0545

    Added the profile page

--- Frontend/src/components/Profile.tsx
+++ Frontend/src/components/Profile.tsx
@@ -1,10 +1,169 @@
-const Profile = ()=>{
-    return(
-        <form className="max-w-7xl  mx-auto my-5 ">
-            Profile
+import { AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import { Avatar } from "./ui/avatar";
+import {
+  Loader2,
+  Locate,
+  Mail,
+  MapPin,
+  MapPinHouse,
+  Plus,
+} from "lucide-react";
+import { useRef, useState, type FormEvent } from "react";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+import { Button } from "./ui/button";
 
-        </form>
-    )
-}
+const Profile = () => {
+  const [profileData, setProfileData] = useState({
+    fullname: "",
+    email: "",
+    phone: "",
+    address: "",
+    city: "",
+    country: "",
+    profilePicture: "",
+  });
 
-export default Profile
+  const imageRef = useRef<HTMLInputElement | null>(null);
+  const [selectedProfilePicture, setSelectedProfilePicture] =
+    useState<string>("");
+
+  const loading = false;
+
+  const fileChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      const result = reader.result as string;
+
+      setSelectedProfilePicture(result);
+
+      setProfileData((prevData) => ({
+        ...prevData,
+        profilePicture: result,
+      }));
+    };
+
+    reader.readAsDataURL(file);
+  };
+
+  const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setProfileData({ ...profileData, [name]: value });
+  };
+
+  const updateProfileHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    //api implementation update 
+    console.log(profileData);
+  };
+
+  return (
+    <form onSubmit={updateProfileHandler} className="max-w-7xl mx-auto my-5">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          <Avatar className="relative md:w-28 md:h-28 sm:w-20 sm:h-20 bg-gray-100 rounded-full flex items-center justify-center">
+            <AvatarImage src={selectedProfilePicture} />
+            <AvatarFallback>PK</AvatarFallback>
+
+            <input
+              ref={imageRef}
+              type="file"
+              className="hidden"
+              accept="image/*"
+              onChange={fileChangeHandler}
+            />
+
+            <div
+              onClick={() => imageRef.current?.click()}
+              className="absolute inset-0 z-10 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-300 bg-black bg-opacity-50 rounded-full cursor-pointer"
+            >
+              <Plus className="text-red-700 w-8 h-8" />
+            </div>
+          </Avatar>
+
+          {/* FULLNAME FIXED */}
+          <Input
+            type="text"
+            name="fullname"
+            value={profileData.fullname}
+            onChange={changeHandler}
+            className="font-bold text-2xl outline-none border-none focus-visible:ring-transparent"
+          />
+        </div>
+      </div>
+
+      <div className="grid md:grid-cols-4 md:gap-2 gap-3 my-10">
+        <div className="flex items-center gap-4 rounded-sm p-2 bg-gray-200">
+          <Mail className="text-gray-500" />
+          <div className="w-full">
+            <Label>Email</Label>
+            <input
+              name="email"
+              value={profileData.email}
+              onChange={changeHandler}
+              className="w-full text-gray-600 bg-transparent focus-visible:ring-0 outline-none border-none"
+            />
+          </div>
+        </div>
+
+        <div className="flex items-center gap-4 rounded-sm p-2 bg-gray-200">
+          <Locate className="text-gray-500" />
+          <div className="w-full">
+            <Label>Address</Label>
+            <input
+              name="address"
+              value={profileData.address}
+              onChange={changeHandler}
+              className="w-full text-gray-600 bg-transparent focus-visible:ring-0 outline-none border-none"
+            />
+          </div>
+        </div>
+
+        <div className="flex items-center gap-4 rounded-sm p-2 bg-gray-200">
+          <MapPin className="text-gray-500" />
+          <div className="w-full">
+            <Label>City</Label>
+            <input
+              name="city"
+              value={profileData.city}
+              onChange={changeHandler}
+              className="w-full text-gray-600 bg-transparent focus-visible:ring-0 outline-none border-none"
+            />
+          </div>
+        </div>
+
+        <div className="flex items-center gap-4 rounded-sm p-2 bg-gray-200">
+          <MapPinHouse className="text-gray-500" />
+          <div className="w-full">
+            <Label>Country</Label>
+            <input
+              name="country"
+              value={profileData.country}
+              onChange={changeHandler}
+              className="w-full text-gray-600 bg-transparent focus-visible:ring-0 outline-none border-none"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="text-center">
+        {loading ? (
+          <Button disabled className="bg-button hover:bg-button-hover">
+            <Loader2 className="mr-2 w-4 h-4 animate-spin" />
+            Please Wait…
+          </Button>
+        ) : (
+          // FIXED VISIBLE BUTTON
+          <Button className="bg-button hover:bg-button-hover">
+            Update Profile
+          </Button>
+        )}
+      </div>
+    </form>
+  );
+};
+
+export default Profile;
